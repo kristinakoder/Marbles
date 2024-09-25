fc1a1889047cb16d49cd3af9dac4f215a24c7e04
kristinakoder <kristina.g.olsen@gmail.com>
2024-09-25 19:53:04 +0200

    finished inputmap for click-and-drag

--- Assets/_Scripts/Interact.cs
+++ Assets/_Scripts/Interact.cs
@@ -1,17 +1,58 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Interact : MonoBehaviour
 {
-    private Marble selectedMarble;
+    private Marble selectedMarble = null;
     private Camera mainCamera;
-    [SerializeField] private Material mat0;
 
     private InputControls input;
+    private Vector3 screenPos;
+
+    private Vector3 WorldPos
+    {
+        get
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            Plane plane = new(Vector3.up, Vector3.zero);
+            plane.Raycast(ray, out float distance);
+            return ray.GetPoint(distance - 5);
+        }
+    }
+
+    private bool HitMarble
+    {
+        get
+        {
+            Ray ray = mainCamera.ScreenPointToRay(screenPos);
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                selectedMarble = hit.collider.GetComponent<Marble>();
+            }
+            return selectedMarble != null;
+        }
+    }
     
     void Awake()
     {
+        mainCamera = Camera.main;
         input = new InputControls();
+        input.Interacting.Position.performed += ctx => { screenPos = ctx.ReadValue<Vector2>(); };
+        input.Interacting.Press.performed += _ => 
+        { 
+            screenPos = input.Interacting.Position.ReadValue<Vector2>();
+            if(HitMarble) StartCoroutine(MoveMarble()); 
+        };
+        input.Interacting.Press.canceled += _ => selectedMarble = null;
+    }
+
+    private IEnumerator MoveMarble()
+    {
+        while(selectedMarble != null)
+        {
+            selectedMarble.MoveMarble(WorldPos);
+            yield return null;
+        }
     }
 
     private void OnEnable()
@@ -23,71 +64,4 @@ public class Interact : MonoBehaviour
     {
         input.Disable();
     }
-
-    void Start()
-    {
-        mainCamera = Camera.main;
-        input.Interacting.Press.started += ctx => StartPress(ctx);
-        input.Interacting.Press.canceled += ctx => EndPress(ctx);
-    }
-
-    private void StartPress(InputAction.CallbackContext ctx)
-    {
-        Debug.Log("start press");
-    }
-
-    private void EndPress(InputAction.CallbackContext ctx)
-    {
-        Debug.Log("end press");
-    }
-
-    void Update()
-    {
-        //HandleInput();
-    }
-
-    /*
-    private void HandleInput()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            GetSelection();
-        }
-
-        if (selectedMarble != null && Input.GetMouseButton(0))
-        {
-            MoveMarble();
-        }
-        
-        //how to change the color of a material. To be used in menu where players can choose colors.
-        if (Input.GetMouseButtonDown(1))
-        {
-            mat0.color = new Color(1,0,1);
-        }
-    }
-
-    private void GetSelection()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
-        {
-            selectedMarble = hit.collider.GetComponent<Marble>();
-        }
-    }
-
-    private void MoveMarble()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        Plane plane = new(Vector3.up, Vector3.zero);
-        float distance;
-
-        if (plane.Raycast(ray, out distance))
-        {
-            Vector3 wordlMousePos = ray.GetPoint(distance - 5);
-            selectedMarble.MoveMarble(wordlMousePos);
-        }
-    }
-    */
 }
